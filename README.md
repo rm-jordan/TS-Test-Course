74228480678e36687eb09ded8f4f4b3de1608a9f
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2023-09-02 09:17:15 -0400

    update test and readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 TS Test Course
+
+Structure of a Properly Written Unit Test:
+
+AAA Principles:
+
+- arrange
+- act
+- assert
+
+Setup
+Teardown
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -3,9 +3,16 @@ import { toUpperCase } from "../app/Utils"
 
 describe('Utils test suite', () => {
 
-  test('should return uppercase', ()=> {
-    const result  = toUpperCase('abc')
-    expect(result).toBe('ABC')
+  it('should return uppercase of a valid string', () => {
+    // arrange:
+    const sut = toUpperCase
+    const expected = "ABC"
+
+    // act:
+    const actual  = toUpperCase('abc')
+
+    // assert:
+    expect(actual).toBe('ABC')
   })
 
 })
