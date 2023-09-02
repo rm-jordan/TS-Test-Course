74b44bc563f014ae84b72ade22e759ece0517807
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2023-09-02 09:49:04 -0400

    jest assertions and matchers

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils"
+import { getStringInfo, toUpperCase } from "../app/Utils"
 
 
 describe('Utils test suite', () => {
@@ -13,6 +13,36 @@ describe('Utils test suite', () => {
 
     // assert:
     expect(actual).toBe('ABC')
+  })
+
+  it.only('should return info for valid string', () => {
+
+
+    const actual = getStringInfo('My-String')
+
+    // toBe - primitve types
+    expect(actual.lowerCase).toBe('my-string')
+    // toEqual 
+    expect(actual.extraInfo).toEqual({})
+
+    // toHaveLength - cleaner to read matcher
+    expect(actual.characters).toHaveLength(9)
+
+    expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g'])
+    expect(actual.characters).toContain<string>('M')
+
+    //  characters not in order
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g','M', 'y', '-'])
+    )
+
+    expect(actual.extraInfo).not.toBe(undefined)
+    expect(actual.extraInfo).not.toBeUndefined()
+    expect(actual.extraInfo).toBeDefined()
+
+    // not sure what forms are but make sure its valid
+    expect(actual.extraInfo).toBeTruthy()
+
   })
 
 })
